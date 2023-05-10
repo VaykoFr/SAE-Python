164fefe0e7c481b3f95f228e335695f8f862546a
Nicolas Tassin <nicolas.tassin23@gmail.com>
2023-05-10 10:23:13 +0200

    Ajout d'un calendrier + séléction auto

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 from PIL import Image
+import datetime
 
 st.set_page_config(page_title='Analyse des données ticket')
 st.header('Bienvenu sur ma page de statistique')
@@ -35,9 +36,14 @@ df_carte_fidelite = pd.read_excel(excel_file,
                                   usecols='A:M',
                                   header=0)
 
+
+st.header("Feuille Tikcet du fichier excel")
 st.dataframe(df_ticket)
+st.header("Feuille Détail Ticket du fichier Excel")
 st.dataframe(df_detail_ticket)
+st.header("Feuille Produit en Rayon du fichier Excel")
 st.dataframe(df_produit_en_rayon)
+st.header("Feuille Carte Fidélité du fichier Excel")
 st.dataframe(df_carte_fidelite)
 
 
@@ -50,4 +56,46 @@ pie_caisses = px.pie(df_ticket,
                      names='Caisse')
 
 st.plotly_chart(pie_caisses)
+
+date_default = datetime.date(2021, 1, 1)
+date_min = datetime.date(2021, 1, 1)
+date_max = datetime.date(2021, 10, 31)
+
+#Afficher le calendrier 
+
+date_user = st.date_input(label ="Choisissez une date pour plus de détails", 
+              value=date_default, 
+              min_value=date_min,
+              max_value=date_max, 
+              key=None, 
+              help=None, 
+              on_change=None, 
+              args=None, kwargs=None, 
+              disabled=False, 
+             label_visibility="visible")
+
+# Récupérer la date choisie par l'utilisateur
+df_ticket['DateH_Ticket'] = pd.to_datetime(df_ticket['DateH_Ticket'], format='%d/%m/%Y %H:%M:%S')
+df_filtered = df_ticket[df_ticket['DateH_Ticket'].dt.date == date_user]
+
+if df_filtered.empty:
+    st.write(f"Il n'y a aucun ticket pour la date choisie : {date_user}")
+else:
+    # Grouper les tickets par caisse et compter le nombre de tickets pour chaque caisse
+    df_grouped = df_filtered.groupby(['Caisse']).agg({'No_Ticket': 'count'}).reset_index()
+
+    # Trier les caisses par ordre décroissant du nombre de tickets
+    df_sorted = df_grouped.sort_values(by='No_Ticket', ascending=False)
+
+    # Sélectionner les 10 caisses les plus utilisées
+    df_top_10 = df_sorted.head(10)
+
+    # Afficher le camembert
+    pie_caisses = px.pie(df_top_10,
+                         title=f'Caisses les plus utilisées le {date_user}',
+                         values='No_Ticket',
+                         names='Caisse')
+    st.plotly_chart(pie_caisses)
+
+
 
